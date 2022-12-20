b2a98d9b6de31ac78c360baf90b58982a8a09f48
paulb <paulbruno@bk.ru>
2022-12-20 18:35:34 +0300

    delete example service and controller, change module, adding mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import {Module} from "@nestjs/common";
-import {AppController} from "./app.controller";
-import {AppService} from "./app.service";
+import {TrackModule} from "./track/app.module";
+import {MongooseModule} from "@nestjs/mongoose";
 
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        TrackModule,
+        // MongooseModule.forRoot('mongodb://localhost/5000'),
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import {Get, Injectable} from "@nestjs/common";
-
-@Injectable()
-export class AppService {
-    @Get()
-    getUsers() : string {
-        return 'GET ALL USERS';
-    }
-}
