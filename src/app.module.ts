8003732444c58e7521bc4ba4e9c68d9c359dea00
paulb <paulbruno@bk.ru>
2022-12-25 15:30:08 +0300

    connect to mongo DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import {Module} from "@nestjs/common";
-import {TrackModule} from "./track/app.module";
-import {MongooseModule} from "@nestjs/mongoose";
+import { Module } from "@nestjs/common";
+import { TrackModule } from "./track/track.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 
 @Module({
     imports: [
         TrackModule,
-        // MongooseModule.forRoot('mongodb://localhost/5000'),
+        MongooseModule.forRoot("mongodb+srv://PaulBruno:RX9YdiG4ousZwBec@cluster0.jte7kjz.mongodb.net/?retryWrites=true&w=majority")
     ]
 })
 export class AppModule {
