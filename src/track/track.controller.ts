af89e059b0f502408112a4a309e3a2ae4849540b
paulb <paulbruno@bk.ru>
2022-12-29 23:32:07 +0300

    Adding logic and realise it

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,17 +1,25 @@
 import { TrackService } from "./track.service";
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { CreateTrackDto } from "./dto/create-track.dto";
+import mongoose from "mongoose";
 
 @Controller("/tracks")
 export class TrackController {
     constructor( private trackService: TrackService ) {
     }
 
-    @Get()
-    getUsers() {
-        return this.trackService.getUsers();
+    @Post()
+    create(@Body() dto: CreateTrackDto) {
+        return this.trackService.create(dto);
     }
 
-    getCurrentUser() {
-        return this.trackService.getCurrentUser();
+    @Get()
+    getAllTracks() {
+        return this.trackService.getAllTracks();
+    }
+
+    @Get(':id')
+    getCurrentTrack(@Param('id') id: mongoose.ObjectId) {
+        return this.trackService.getCurrentTrack(id);
     }
 }
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Track, TrackDocument } from "./schemas/track.schema";
+import mongoose, { Model } from "mongoose";
+import { Comment, CommentDocument } from "./schemas/comment.schema";
+import { CreateTrackDto } from "./dto/create-track.dto";
 
 
 @Injectable()
 export class TrackService {
-    async getUsers(): Promise<string> {
-        return "get all users";
+    constructor( @InjectModel(Track.name) private trackModel: Model<TrackDocument>, // needed for using model in our service
+                 @InjectModel(Comment.name) private commentModel: Model<CommentDocument> ) {} // to pass to it use "this"
+    async create(dto: CreateTrackDto): Promise<Track> {
+        const createdTrack = new this.trackModel({...dto, listens: 0})
+        return createdTrack.save();
+    }
+    async getAllTracks(): Promise<Track[]> {
+        return this.trackModel.find().exec();
     }
 
-    async getCurrentUser(): Promise<string> {
-        return "get current user";
+    async getCurrentTrack(id: mongoose.ObjectId): Promise<Track[]> {
+        return this.trackModel.find({ _id: id }).exec();
+
     }
 }
