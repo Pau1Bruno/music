79acd2da7f2518c8c0759002465642a5accccfcb
paulb <paulbruno@bk.ru>
2023-02-02 00:00:30 +0300

    add styles and track pages

--- client/layouts/TrackItem.tsx
+++ client/layouts/TrackItem.tsx
@@ -1,17 +1,38 @@
 import React from "react";
 import { ITrack } from "../types/tracks";
+import { Card, Grid } from "@mui/material";
+import styles from "../styles/TrackItem.module.scss";
+import IconButton from "@mui/material/IconButton";
+import { Delete, PauseCircle, PlayArrow } from "@mui/icons-material";
+import { useRouter } from "next/router";
+
 
 interface TrackItemProps {
     track: ITrack,
     active?: boolean,
 }
 
-const TrackItem: React.FC<TrackItemProps> = ( { track, active = false } ) => {
+const TrackItem: React.FC<TrackItemProps> = ( { track, active = true } ) => {
+    const router = useRouter();
+
     return (
-        <div>
-            <h1>Song: { track.name }</h1>
-            <h2>Artist: { track.artist }</h2>
-        </div>
+        <Card className={ styles.track } onClick={() => router.push('tracks/' + track._id)}>
+            <IconButton onClick={(e) => e.stopPropagation()}>
+                { active
+                    ? <PauseCircle />
+                    : <PlayArrow />
+                }
+            </IconButton>
+            <img className={ styles.picture } src={ track.picture } alt={ "track logo" } />
+            <Grid container direction={"column"} style={{width: 200, margin: '0 20px'}}>
+                <div>{ track.name }</div>
+                <div>{ track.artist }</div>
+            </Grid>
+            {active && <div className={ styles.trackTime }> 2/3 </div>}
+            <IconButton onClick={(e) => e.stopPropagation()} className={ styles.delete}>
+                <Delete/>
+            </IconButton>
+        </Card>
     );
 };
 
---
+++ client/pages/tracks/[id].tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TrackPage = () => {
+
+
+    return (
+        <div>
+            Track Page
+        </div>
+    );
+};
+
+export default TrackPage;
