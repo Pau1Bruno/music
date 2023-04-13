b044f703731ef5034bf4572e6bf8d5855ee4efc1
paulb <paulbruno@bk.ru>
2023-04-13 22:38:35 +0300

    Reworked store: now RootState keeps there, changed reducer Delete: legacy code reducer

--- client/hooks/useAction.ts
+++ client/hooks/useAction.ts
@@ -1,9 +1,10 @@
 import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
 import ActionCreators from "../store/action-creators";
+import {AppDispatch} from "../store";
 
 export const useAction = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch<AppDispatch>();
     // Every action creator wrapped into a dispatch call, so they may be invoked directly.
     return bindActionCreators(ActionCreators, dispatch);
 };
--- client/store/index.ts
+++ client/store/index.ts
@@ -1,19 +1,25 @@
-import {configureStore, ThunkDispatch} from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import {createWrapper} from "next-redux-wrapper";
-import {reducer, RootState} from "./reducers";
 import thunk from "redux-thunk";
-import {AnyAction} from "redux";
 import {api} from "./reducers/apiSlice";
+import playerReducer from "./reducers/playerSlice";
 
-export const makeStore = () =>
-    configureStore({
-        reducer,
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, api.middleware)
-    });
+export type RootState = {
+    player: ReturnType<typeof playerReducer>;
+    [api.reducerPath]: ReturnType<typeof api.reducer>;
+}
+
+export const makeStore = () => configureStore
+({
+    reducer: {
+        player: playerReducer,
+        [api.reducerPath]: api.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, api.middleware)
+});
 
 export type AppStore = ReturnType<typeof makeStore>;
+export type AppDispatch = ReturnType<typeof makeStore>["dispatch"];
 export const wrapper
     =
     createWrapper<AppStore>(makeStore, { debug: true });
-
-export type NextThunkDispatch = ThunkDispatch<RootState, void, AnyAction>;
