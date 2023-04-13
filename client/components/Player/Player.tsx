f1c1c1dd83f57988d97a1627ec18a68c93a2bddb
paulb <paulbruno@bk.ru>
2023-04-13 22:55:19 +0300

    Remove: all logic from Player.tsx Add: custom Hook useSetAudio.ts with redux for Player.tsx Fix: track doesn't stop after the end

--- client/components/Player/Player.tsx
+++ client/components/Player/Player.tsx
@@ -1,77 +1,22 @@
-import React, {useEffect} from "react";
+import React from "react";
 import IconButton from "@mui/material/IconButton";
 import {Pause, PlayArrow, VolumeUp} from "@mui/icons-material";
 import styles from "./Player.module.scss";
 import TrackProgress from "./TrackProgress";
-import {useTypedSelector} from "../../hooks/useTypedSelector";
-import {useAction} from "../../hooks/useAction";
 import VolumeProgress from "./VolumeProgress";
-import {useAddListenMutation} from "../../store/reducers/apiSlice";
-
-let audio: HTMLAudioElement; // declare variable, browser API
+import useAudioPlayer from "../../hooks/useSetAudio";
 
 const Player = () => {
-    const { active, pause, volume, currentTime, duration } = useTypedSelector(state => state.player);
-    const { playTrack, pauseTrack, setVolume, setCurrentTime, setDuration } = useAction();
-    const [ addListen ] = useAddListenMutation();
-
-    useEffect(() => {
-        if (!audio) {
-            audio = new Audio();
-        } else setAudio();
-    }, [ active ]);
-
-    const setAudio = () => {
-        if (active) {
-            audio.src = `http://localhost:5000/${active.audio}`;
-            audio.volume = volume / 100;
-
-            // As track downloaded
-            audio.onloadedmetadata = () => {
-                setDuration(Math.trunc(audio.duration));
-            };
-
-            // Add listen to a server after track is ended / autoplay
-            audio.onended = () => {
-                addListen(active._id);
-                audio.play();
-            };
-
-            // On playing track
-            audio.ontimeupdate = () => {
-                setCurrentTime(Math.trunc(audio.currentTime));
-            };
-        }
-    };
-
-    // для того, чтобы пауза и проигрывание работали из списка
-    useEffect(() => {
-        if (!pause) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    }, [ pause ]);
-
-    const play = () => {
-        if (pause) {
-            playTrack();
-            audio.play();
-        } else {
-            pauseTrack();
-            audio.pause();
-        }
-    };
-
-    const changeVolume = (e: React.ChangeEvent<HTMLInputElement>) => {
-        audio.volume = Number(e.target.value) / 100;
-        setVolume(Number(e.target.value));
-    };
-
-    const changeCurrentTime = (e: React.ChangeEvent<HTMLInputElement>) => {
-        audio.currentTime = Number(e.target.value);
-        setCurrentTime(Math.trunc(audio.currentTime));
-    };
+    const {
+        active,
+        pause,
+        volume,
+        currentTime,
+        duration,
+        changeCurrentTime,
+        changeVolume,
+        play
+    } = useAudioPlayer();
 
     // Если трек не выбран, то плеера не будет видно
     if (!active) {
@@ -81,6 +26,7 @@ const Player = () => {
     return (
         <div className={styles.player}>
             <div className={styles.left}>
+
                 <IconButton onClick={play} className={styles.play_pause}>
                     {pause
                         ? <PlayArrow />
