12df0397c8e0b5ec198c41bc5ce2a0658029b0aa
Pau1Bruno <paulbruno@bk.ru>
2023-04-25 23:22:54 +0300

    Merge remote-tracking branch 'origin/v1.0.0-initial-release' into v1.0.0-initial-release
    # Conflicts:
    #	client/components/Modal/Modal.tsx

--- client/components/Modal/Modal.tsx
+++ client/components/Modal/Modal.tsx
@@ -24,29 +24,5 @@ const Modal: FC<ModalProps> = ({ children, modal, setModal }) => {
     );
 };
 
-<<<<<<< HEAD
-export default Modal;
-=======
 export default Modal;
 
-//import React from 'react';
-// import classes from "./MyModal.module.css";
-//
-// const MyModal = ({children, visible, setVisible}) => {
-//
-//     const rootClasses = [classes.myModal];
-//     if (visible) {
-//         rootClasses.push(classes.active);
-//     }
-//
-//     return (
-//         <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
-//             <div className={classes.myModalContent} onClick={event => event.stopPropagation()}>
-//                 {children}
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default MyModal;
->>>>>>> origin/v1.0.0-initial-release
